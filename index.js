1b54a4989aaac4663df6b4dacfdeb6cfb14eaa0e
amirhnajafiz <najafizadeh21@gmail.com>
2022-10-01 10:09:15 +0330

    add: http client class

--- index.js
+++ index.js
@@ -1,60 +1,62 @@
-function fetchgetrequest() {
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-        .then(response => response.json())
-        .then(json => console.log(json))
-}
+class HTTPClient {
+    fetchgetrequest() {
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+            .then(response => response.json())
+            .then(json => console.log(json))
+    }
 
-function fetchpostrequest() {
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST',
-        body: JSON.stringify({
-            title: 'foo',
-            body: 'bar',
-            userId: 1
-        }),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
-    })
-        .then(response => response.json())
-        .then(json => console.log(json))
-}
+    fetchpostrequest() {
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify({
+                title: 'foo',
+                body: 'bar',
+                userId: 1
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+            .then(response => response.json())
+            .then(json => console.log(json))
+    }
 
-function fetchputrequest() {
-    fetch('https://jsonplaceholder.typicode.com/posts/1', {
-        method: 'PUT',
-        body: JSON.stringify({
-            id: 1,
-            title: 'foo',
-            body: 'bar',
-            userId: 1
-        }),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
-    })
-        .then(response => response.json())
-        .then(json => console.log(json))
-}
+    fetchputrequest() {
+        fetch('https://jsonplaceholder.typicode.com/posts/1', {
+            method: 'PUT',
+            body: JSON.stringify({
+                id: 1,
+                title: 'foo',
+                body: 'bar',
+                userId: 1
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+            .then(response => response.json())
+            .then(json => console.log(json))
+    }
 
-function fetchpatchrequest() {
-    fetch('https://jsonplaceholder.typicode.com/posts/1', {
-        method: 'PATCH',
-        body: JSON.stringify({
-            title: 'foo'
-        }),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
-    })
-        .then(response => response.json())
-        .then(json => console.log(json))
-}
+    fetchpatchrequest() {
+        fetch('https://jsonplaceholder.typicode.com/posts/1', {
+            method: 'PATCH',
+            body: JSON.stringify({
+                title: 'foo'
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+            .then(response => response.json())
+            .then(json => console.log(json))
+    }
 
-function fetchdeleterequest() {
-    fetch('https://jsonplaceholder.typicode.com/posts/1', {
-        method: 'DELETE'
-    })
-        .then(response => response.json())
-        .then(json => console.log(json))
+    fetchdeleterequest() {
+        fetch('https://jsonplaceholder.typicode.com/posts/1', {
+            method: 'DELETE'
+        })
+            .then(response => response.json())
+            .then(json => console.log(json))
+    }
 }
